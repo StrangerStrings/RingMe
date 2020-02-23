70711da5a6ba4c97335d4d368a81d53cf0dd4c9d
Ben Clayton-Green <ben-clayton-green@Bens-MacBook-Air.local>
2020-02-23 21:41:47 +0000

    better now, long press and animations

--- script.js
+++ script.js
@@ -3,20 +3,37 @@
 //         -   - - - - - - -   -
 
 const Main = document.getElementsByTagName('main')[0]
+let TimeTilDeletion = 2000
 
-createExpandingRing();
+window.addEventListener('keydown', ()=>{
+    if (event.key == ' '){
+        createExpandingRing()
+    }
 
+window.addEventListener('keyup', ()=>{
+    if (event.key == ' '){
+        createExpandingMiddle()
+    }
+})
 function createExpandingRing() {
 
     const ring = document.createElement('article')
     Main.appendChild(ring)
-    waitThen(() => {ring.classList.add('expandingRing')})
-
+    setTimeout(()=>{
+        ring.classList.add('expanding')
+    },35)
+    waitThenDispose(ring)
+}
+function createExpandingMiddle(){
     const middleBit = document.createElement('section')
     Main.appendChild(middleBit)
-    waitThen(() => {middleBit.classList.add('expandingRing')})
+    middleBit.classList.add('expanding')
+    waitThenDispose(middleBit)
 }
 
-function waitThen(fnc) {
-    setTimeout(fnc,1)
+
+function waitThenDispose(div){
+    setTimeout(() => {
+        Main.removeChild(div)
+    },TimeTilDeletion)
 }
